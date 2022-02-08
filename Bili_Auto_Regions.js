3faf53f1f370b0d4b47722260d6be1b1f07f3386
NekoEffect <34155807+NekoEffect@users.noreply.github.com>
2022-02-08 19:56:35 +0800

    Update Bili_Auto_Regions.js

--- Bili_Auto_Regions.js
+++ Bili_Auto_Regions.js
@@ -16,23 +16,34 @@ async function SwitchRegion(play) {
 	const CN = $.read('BiliArea_CN') || 'DIRECT'; //Your China sub-policy name.
 	const TW = $.read('BiliArea_TW') || '台湾节点'; //Your Taiwan sub-policy name.
 	const HK = $.read('BiliArea_HK') || '香港节点'; //Your HongKong sub-policy name.
+	const off = $.read('BiliArea_disabled') || ''; //WiFi blacklist(disable region change), separated by commas.
 	const current = await $.getPolicy(Group);
 	const area = (() => {
+		let select;
 		if (/\u50c5[\u4e00-\u9fa5]+\u6e2f|%20%E6%B8%AF&/.test(play)) {
-			if (current != HK) return HK;
+			const test = /\u50c5[\u4e00-\u9fa5]+\u53f0/.test(play);
+			if (current != HK && (current == TW && test ? 0 : 1)) select = HK;
 		} else if (/\u50c5[\u4e00-\u9fa5]+\u53f0|%20%E5%8F%B0&/.test(play)) {
-			if (current != TW) return TW;
-		} else if (current != CN) return CN;
+			if (current != TW) select = TW;
+		} else if (play === -404) {
+			if (current != DF) select = DF;
+		} else if (current != CN) {
+			select = CN;
+		}
+		if ($.isQuanX && current === 'direct' && select === 'DIRECT') {
+			select = null; //avoid loops in some cases
+		}
+		return select;
 	})()
 
-	if (area) {
+	if (area && !off.includes($.ssid || undefined)) {
 		const change = await $.setPolicy(Group, area);
-		const notify = $.read('BiliAreaNotify') === 'false';
+		const notify = $.read('BiliAreaNotify') === 'true';
 		const msg = SwitchStatus(change, current, area);
 		if (!notify) {
-			$.notify(/^http/.test(play) || !play ? `` : play, ``, msg);
+			$.notify((/^(http|-404)/.test(play) || !play) ? `` : play, ``, msg);
 		} else {
-			console.log(`${/^http/.test(play)||!play?``:play}\n${msg}`);
+			console.log(`${(/^(http|-404)/.test(play)||!play)?``:play}\n${msg}`);
 		}
 		if (change) {
 			return true;
@@ -42,7 +53,7 @@ async function SwitchRegion(play) {
 }
 
 function SwitchStatus(status, original, newPolicy) {
-	if (status) {
+	if (status && typeof original !== 'number') {
 		return `${original}  =>  ${newPolicy}  =>  🟢`;
 	} else if (original === 2) {
 		return `切换失败, 策略组名未填写或填写有误 ⚠️`
@@ -59,9 +70,14 @@ function EnvInfo() {
 	if (typeof($response) !== 'undefined') {
 		const raw = JSON.parse($response.body);
 		const data = raw.data || raw.result || {};
-		//if surge or loon, $done() will auto reconnect with the new policy
-		SwitchRegion(data.title)
-			.then(s => s && !$.isQuanX ? $done() : QueryRating(raw, data));
+		SwitchRegion(data.title || (raw.code === -404 ? -404 : null))
+			.then(s => s ? $done({
+				status: $.isQuanX ? "HTTP/1.1 408 Request Timeout" : 408,
+				headers: {
+					Connection: "close"
+				},
+				body: "{}"
+			}) : QueryRating(raw, data));
 	} else {
 		const raw = $request.url;
 		const res = {
@@ -80,7 +96,10 @@ async function QueryRating(body, play) {
 				GetRawInfo(play.origin_name)
 			]);
 			const exYear = body.data.publish.release_date_show.split(/^(\d{4})/)[1];
-			const filterInfo = [play.title, play.origin_name, play.staff.info + play.actor.info, exYear];
+			const info1 = (play.staff && play.staff.info) || '';
+			const info2 = (play.actor && play.actor.info) || '';
+			const info3 = (play.celebrity && play.celebrity.map(n => n.name).join('/')) || '';
+			const filterInfo = [play.title, play.origin_name, info1 + info2 + info3, exYear];
 			const [rating, folk, name, id, other] = ExtractMovieInfo([...t1, ...t2], filterInfo);
 			const limit = JSON.stringify(body.data.modules)
 				.replace(/"\u53d7\u9650"/g, `""`).replace(/("area_limit":)1/g, '$10');
@@ -170,6 +189,17 @@ function nobyda() {
 	const isLoon = typeof $loon != "undefined";
 	const isQuanX = typeof $task != "undefined";
 	const isSurge = typeof $network != "undefined" && typeof $script != "undefined";
+	const ssid = (() => {
+		if (isQuanX && typeof($environment) !== 'undefined') {
+			return $environment.ssid;
+		}
+		if (isSurge && $network.wifi) {
+			return $network.wifi.ssid;
+		}
+		if (isLoon) {
+			return JSON.parse($config.getConfig()).ssid;
+		}
+	})();
 	const notify = (title, subtitle, message) => {
 		console.log(`${title}\n${subtitle}\n${message}`);
 		if (isQuanX) $notify(title, subtitle, message);
@@ -261,6 +291,7 @@ function nobyda() {
 		isLoon,
 		notify,
 		read,
+		ssid,
 		get
 	}
 }
